bfced0196e3e981e0e4f774799252fd01914b18f
DennisGL1 <dennis@gamelevelone.com>
2018-07-16 17:52:23 +0700

    - create Portal system and level data system (to instantiate player and to move scene - save scenes

---
+++ Assets/_MAIN/Scripts/Systems/LevelDataSystem.cs
@@ -0,0 +1,32 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class LevelDataSystem : ComponentSystem {
+
+	public struct Component{
+		public LevelData levelData;
+	}
+
+	// void OnLevelWasLoaded(int level)
+	// {
+	// 	this.Enabled = true;
+	// }
+
+	protected override void OnUpdate()
+	{
+		foreach(var e in GetEntities<Component>()){
+			if(!e.levelData.isInitialied){
+				InitializeLevelData(e);
+			}
+		}
+		
+	}
+
+	void InitializeLevelData(Component e)
+	{
+		e.levelData.isInitialied = true;
+		GameObject.Instantiate(e.levelData.playerObj,e.levelData.playerStartPos,Quaternion.identity);
+
+		// this.Enabled = false;
+	}
+}
---
+++ Assets/_MAIN/Scripts/Systems/PortalSystem.cs
@@ -0,0 +1,25 @@
+﻿using Unity.Entities;
+using UnityEngine.SceneManagement;
+
+public class PortalSystem : ComponentSystem {
+
+	
+	public struct Component{
+		public Portal portal;
+	}
+
+	protected override void OnUpdate()
+	{
+		foreach(var e in GetEntities<Component>()){
+			if(e.portal.triggered){
+				ChangeMap(e);
+			}
+		}
+		
+	}
+
+	void ChangeMap(Component e)
+	{
+		SceneManager.LoadScene(e.portal.sceneDestination);
+	}
+}
