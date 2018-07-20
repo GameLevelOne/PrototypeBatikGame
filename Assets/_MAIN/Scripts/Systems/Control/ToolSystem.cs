060a76743c252d838eaeeb476e2f2bce5828fc37
DennisGL1 <dennis@gamelevelone.com>
2018-07-20 15:43:38 +0700

    - modified toolsystem

--- Assets/_MAIN/Scripts/Systems/Control/ToolSystem.cs
+++ Assets/_MAIN/Scripts/Systems/Control/ToolSystem.cs
@@ -10,6 +10,12 @@ public class ToolSystem : ComponentSystem {
 	protected override void OnUpdate()
 	{
 		//if player input button action, do use tool.
+		foreach(var e in GetEntities<ToolSystemComponent>())
+		{
+			UseTool(e.currentTool);
+			this.Enabled = false;
+		}
+
 	}
 
 	void UseTool(ToolType toolType)
@@ -18,39 +24,39 @@ public class ToolSystem : ComponentSystem {
 		
 
 		if(toolType == ToolType.Bow){
-
+			UseBow();
 		}else if(toolType == ToolType.Hook){
-
+			UseHook();
 		}else if(toolType == ToolType.Bomb){
-			
+			UseBomb();
 		}else if(toolType == ToolType.Hammer){
-			
+			UseHammer();
 		}else if(toolType == ToolType.Net){
-			
+			UseNet();
 		}else if(toolType == ToolType.FishingRod){
-			
+			UseFisingRod();
 		}else if(toolType == ToolType.Container1){
-			
+			UseContainer1();
 		}else if(toolType == ToolType.Container2){
-			
+			UseContainer2();
 		}else if(toolType == ToolType.Container3){
-			
+			UseContainer3();
 		}else if(toolType == ToolType.Container4){
-			
+			UseContainer4();
 		}else if(toolType == ToolType.Shovel){
-			
+			UseShovel();
 		}else if(toolType == ToolType.Lantern){
-			
+			UseLantern();
 		}else if(toolType == ToolType.InvisibilityCloak){
-			
+			UseInvisibilityCloack();
 		}else if(toolType == ToolType.MagicMedallion){
-			
+			UseMagicMedallion();
 		}else if(toolType == ToolType.FastTravel){
-			
+			UseFastTravel();
 		}else if(toolType == ToolType.Flippers){
-			
+			UseFlippers();
 		}else if(toolType == ToolType.Boots){
-			
+			UseBoots();
 		}
 	}
 
