dea6fb6565aeb58daf04864b47c663fc1fc8af92
DennisGL1 <dennis@gamelevelone.com>
2018-07-20 13:17:31 +0700

    - added some systems and constants

---
+++ Assets/_MAIN/Scripts/Constants.cs
@@ -0,0 +1,44 @@
+﻿/// <summary>
+/// Contains all constant strings like tags, animator parameters, sorting layer names, and playerpref keys
+/// </summary>
+public static class Constants {
+
+	public static class AnimatorParameter
+	{
+		public static class Int
+		{
+			
+		}
+
+		public static class Float
+		{
+
+		}
+
+		public static class Trigger
+		{
+			
+		}
+
+		public static class Bool
+		{
+
+		}
+
+	}
+
+	public static class SortingLayerName
+	{
+
+	}
+
+	public static class Tag
+	{
+
+	}
+
+	public static class PlayerPrefKey
+	{
+
+	}
+}
---
+++ Assets/_MAIN/Scripts/Systems/Object/PullPushSystem.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PullPushSystem : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
