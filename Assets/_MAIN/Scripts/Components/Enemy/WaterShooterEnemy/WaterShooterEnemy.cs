4ee2f121157e85261dc8ab21c10918230c4100b0
DennisGL1 <dennis@gamelevelone.com>
2018-08-08 12:40:39 +0700

    - add water enemy

---
+++ Assets/_MAIN/Scripts/Components/Enemy/WaterShooterEnemy/WaterShooterEnemy.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class WaterShooterEnemy : MonoBehaviour {
+	[HeaderAttribute("WaterShooterEnemy Attributes")]
+	public Enemy enemy;
+	public TriggerDetection playerTriggerDetection;
+
+	void OnEnable()
+	{
+		playerTriggerDetection.OnTriggerEnter += SetPlayerTransform;
+	}
+
+	void OnDisable()
+	{
+		playerTriggerDetection.OnTriggerEnter -= SetPlayerTransform;
+	}
+
+
+	void SetPlayerTransform(GameObject player)
+	{
+		enemy.playerTransform = player.transform;
+	}
+}
