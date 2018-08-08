ce84b794c00673f8c732b8c71827024903d2bdc0
Bhakti <bhakti.gamelevelone@gmail.com>
2018-08-08 11:23:04 +0700

    Fish movement system (without animation)

--- Assets/_MAIN/Scripts/Components/Tool/FishCollectible.cs
+++ Assets/_MAIN/Scripts/Components/Tool/FishCollectible.cs
@@ -8,6 +8,7 @@ public enum FishCollectibleType {
 
 public enum FishState {
 	IDLE,
+	PATROL,
 	CHASE,
 	CATCH,
 	FLEE
@@ -17,9 +18,20 @@ public class FishCollectible : MonoBehaviour {
 	public FishCollectibleType type;
 	public FishState state;
 
-	public GameObject targetBait;
+	public Collider2D parentPoolCol;
 	public FishingRod fishingRod;
 
+	public Vector2 targetPos;
 	public float moveSpeed;
+	public float chaseSpeed;
+	public float fleeSpeed;
 	public float timeToCatch;
+	public float idleDuration;
+
+	[SerializeField] float timeIdle;
+
+	public float TimeIdle {
+		get {return timeIdle;}
+		set {timeIdle = value;}
+	}
 }
--- Assets/_MAIN/Scripts/Systems/Object/FishCollectibleSystem.cs
+++ Assets/_MAIN/Scripts/Systems/Object/FishCollectibleSystem.cs
@@ -5,10 +5,13 @@ public class FishCollectibleSystem : ComponentSystem {
 	public struct FishCollectibleData {
 		public readonly int Length;
 		public ComponentArray<FishCollectible> FishCollectible;
+		public ComponentArray<Rigidbody2D> Rigidbody;
 	}
 	[InjectAttribute] FishCollectibleData fishCollectibleData;
 
 	FishCollectible fishCollectible;
+
+	Rigidbody2D rigidbody;
 	
 	float timer;
 	float deltaTime;
@@ -20,36 +23,82 @@ public class FishCollectibleSystem : ComponentSystem {
 
 		for (int i=0; i<fishCollectibleData.Length; i++) {
 			fishCollectible = fishCollectibleData.FishCollectible[i];
-
-			//CHASE first before CATCH
-			if (fishCollectible.state == FishState.CATCH) {
-				WaitingToCatch ();
-			}
+			rigidbody = fishCollectibleData.Rigidbody[i];
 			
+			if (fishCollectible.state == FishState.IDLE) {
+				Idle ();
+			} else if (fishCollectible.state == FishState.PATROL) {
+				Patrol ();
+			} else if (fishCollectible.state == FishState.CHASE) {
+				ChaseBait ();
+			} else if (fishCollectible.state == FishState.CATCH) {
+				WaitingToCatch ();
+			} else if (fishCollectible.state == FishState.FLEE) {
+				Flee ();
+			}
+		}
+	}
+
+	void Idle () {
+		if (fishCollectible.TimeIdle <= 0f) {
+			fishCollectible.targetPos = RandomPosition ();
+			fishCollectible.state = FishState.PATROL;
+		} else {
+			fishCollectible.TimeIdle -= deltaTime;
+		}
+	}
+
+	void Patrol () {
+		if (Vector2.Distance(fishCollectible.targetPos, rigidbody.position) < 0.1f) {
+			fishCollectible.TimeIdle = fishCollectible.idleDuration;
+			fishCollectible.state = FishState.IDLE;
+		} else {
+			Move (rigidbody.position, fishCollectible.targetPos, fishCollectible.moveSpeed);
+		}
+	}
+
+	void ChaseBait () {
+		if (Vector2.Distance(fishCollectible.targetPos, rigidbody.position) < 0.1f) {
+			fishCollectible.state = FishState.CATCH;
+		} else {
+			Move (rigidbody.position, fishCollectible.targetPos, fishCollectible.chaseSpeed);
 		}
 	}
 
 	void WaitingToCatch () {
-		if (fishCollectible.fishingRod.state == FishingRodState.STAY) {
-			if (timer < fishCollectible.timeToCatch) {
-				timer += deltaTime;
-				Debug.Log(timer);
-			} else {
-				timer = 0f;
-				Debug.Log("Flee");
-				fishCollectible.state = FishState.FLEE;
-				timer = 0;
-			}
-		} 
-		// else if (fishCollectible.fishingRod.state == FishingRodState.RETURN) {
-		// 	if (fishCollectible.state == FishState.CATCH) {
-				
-		// 	}
-		// } 
+		if (timer < fishCollectible.timeToCatch) {
+			timer += deltaTime;
+		} else {
+			timer = 0f;
+			fishCollectible.targetPos = RandomPosition ();
+			fishCollectible.state = FishState.FLEE;
+		}
+	}
+
+	void Flee () {
+		if (Vector2.Distance(fishCollectible.targetPos, rigidbody.position) < 0.1f) {
+			fishCollectible.TimeIdle = fishCollectible.idleDuration;
+			fishCollectible.state = FishState.IDLE;
+		} else {
+			Move (rigidbody.position, fishCollectible.targetPos, fishCollectible.fleeSpeed);
+		}
 	}
 
 	public void CatchFish (FishCollectible fish) {
 		GameObject.Destroy (fish.gameObject);
 		UpdateInjectedComponentGroups();
+	}
+
+	void Move (Vector2 initPos, Vector2 targetPos, float speed) {
+		rigidbody.position = Vector2.MoveTowards(initPos, targetPos, speed * deltaTime);
+	}
+
+	Vector2 RandomPosition () {
+		float poolRadius = fishCollectible.parentPoolCol.bounds.size.x / 2; //CIRCLE
+		Vector2 poolPos = fishCollectible.parentPoolCol.transform.position;
+		float randomX = poolPos.x + Random.Range(-poolRadius, poolRadius);
+		float randomY = poolPos.y + Random.Range(-poolRadius, poolRadius);
+		
+		return new Vector2 (randomX, randomY);
 	}
 }
