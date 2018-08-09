dc7192796703451ecbdbd4777b9fbaa64eda0a77
DennisGL1 <dennis@gamelevelone.com>
2018-08-09 17:32:33 +0700

    - add enemy armor asset - add enemy armor component - add enemy armor system - save scene

---
+++ Assets/_MAIN/Scripts/Components/Enemy/ArmorEnemy/ArmorEnemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ArmorEnemy : MonoBehaviour {
+	public Enemy enemy;
+	public TriggerDetection playerTriggerDetection;
+	public TriggerDetection hammerTriggerDetection;
+	public Collider2D armorCollider;
+	public bool isArmored = true;
+	public float rollCooldown;
+
+	void OnEnable()
+	{
+
+	}
+
+	void OnDisable()
+	{
+
+	}
+
+	void SetPlayer(GameObject player)
+	{
+		enemy.playerTransform = player.transform;
+	}
+
+	void HitByHammer()
+	{
+		isArmored = false;
+	}
+}
---
+++ Assets/_MAIN/Scripts/Systems/Object/Enemy/ArmorEnemySystem.cs
@@ -0,0 +1,42 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class ArmorEnemySystem : ComponentSystem {
+
+	public struct ArmorEnemyComponent{
+		public readonly int Length;
+		public ComponentArray<Transform> armorEnemyTransform;
+		public ComponentArray<ArmorEnemy> armorEnemy;
+		public ComponentArray<Health> armorEnemyHealth;
+		public ComponentArray<Rigidbody2D> armorEnemyRigidbody;
+		public ComponentArray<Animator> armorEnemyAnim;
+	}
+
+	[InjectAttribute] ArmorEnemyComponent armorEnemyComponent;
+	Transform currArmorEnemyTransform;
+	ArmorEnemy currArmorEnemy;
+	Health currArmorEnemyHealth;
+	Rigidbody2D currArmorEnemyRigidbody;
+	Animator currArmorEnemyAnim;
+
+	protected override void OnUpdate()
+	{
+		// for(int i =0;i<armorEnemyComponent.Length;i++){
+		// 	currArmorEnemyTransform = armorEnemyComponent.armorEnemyTransform[i];
+		// 	currArmorEnemy = armorEnemyComponent.armorEnemy[i];
+		// 	currArmorEnemyHealth = armorEnemyComponent.armorEnemyHealth[i];
+		// 	currArmorEnemyRigidbody = armorEnemyComponent.armorEnemyRigidbody[i];
+		// 	currArmorEnemyAnim = armorEnemyComponent.armorEnemyAnim[i];
+		// }
+	}
+
+	void CheckState()
+	{
+
+	}
+
+	void Idle(){}
+	void Patrol(){}
+	void Roll(){}
+	
+}
