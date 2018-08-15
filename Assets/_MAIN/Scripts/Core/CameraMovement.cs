def3d7f7a5e75e6f07e6c74f6ef5ccc73bcdc15d
DennisGL1 <dennis@gamelevelone.com>
2018-08-15 16:40:59 +0700

    - modified camera system * delete old system * add validation to check camera size (zooming doesn't make camera out of boundaries)
    - save scene

--- Assets/_MAIN/Scripts/Core/CameraMovement.cs
+++ Assets/_MAIN/Scripts/Core/CameraMovement.cs
@@ -2,7 +2,16 @@
 
 public class CameraMovement : MonoBehaviour {
 	public Transform playerTransform;
-	public Vector2 minBound, maxBound;
-	public bool isMovingX, isMovingY;
+	public Vector3 offset;
+	
 	public float smoothSpeed;
+
+	//HOW TO CALCULATE ORTHOGRAPHIC CAMERA WIDTH
+	// void Start()
+	// {
+	// 	float screenHeightInUnits = GetComponent<Camera>().orthographicSize * 2;
+	// 	float screenWidthInUnits = screenHeightInUnits * Screen.width / Screen.height;
+	// 	Debug.Log("screenHeightInUnits = "+screenHeightInUnits);
+	// 	Debug.Log("screenWidthInUnits = "+screenWidthInUnits);
+	// }
 }
