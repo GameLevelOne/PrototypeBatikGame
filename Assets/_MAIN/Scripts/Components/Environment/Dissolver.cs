dadb4da9652eccf40573d018807ae8fdba2b7b00
DennisGL1 <dennis@gamelevelone.com>
2018-09-03 21:57:48 +0700

    - change certain material shaders to dissolve shader - add new dissolver component and system - modified environment prefabs (tree, flower, stone)
    - save scene

---
+++ Assets/_MAIN/Scripts/Components/Environment/Dissolver.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class Dissolver : MonoBehaviour {
+	public List<Renderer> mRenderer;
+	[RangeAttribute(0f,1f)]
+	public List<float> dissolveValue;
+
+	public float dissolveSpeed = 0.25f;
+	public bool init = false;
+	public bool dissolve = false;
+	public bool unDissolve = false;
+}
---
+++ Assets/_MAIN/Scripts/Systems/Area/Environment/DissolverSystem.cs
@@ -0,0 +1,82 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class DissolverSystem : ComponentSystem {
+
+	public struct DissolverComponent{
+		public readonly int Length;
+		public ComponentArray<Dissolver> dissolver;
+	}
+
+	[InjectAttribute] DissolverComponent dissolverComponent;
+	Dissolver currDissolver;
+	float dissolveValue;
+
+	float deltaTime;
+
+	protected override void OnUpdate()
+	{
+		for(int i = 0; i < dissolverComponent.Length; i++){
+			currDissolver = dissolverComponent.dissolver[i];
+			InitDissolver();
+			UpdateDissolver();
+
+			CheckDissolve();
+			CheckUnDissolve();
+		}
+	}
+
+	void InitDissolver()
+	{
+		if(currDissolver.init) return;
+
+		for(int i = 0;i< currDissolver.mRenderer.Count;i++){
+			currDissolver.dissolveValue.Add(currDissolver.mRenderer[i].material.GetFloat("_Level"));
+		}
+		currDissolver.init = true;
+	}
+
+	void UpdateDissolver()
+	{
+		if(currDissolver.init){
+			for(int i = 0;i<currDissolver.mRenderer.Count;i++)
+				currDissolver.mRenderer[i].material.SetFloat("_Level",currDissolver.dissolveValue[i]);
+		}
+	}
+
+	void CheckDissolve()
+	{
+		if(currDissolver.dissolve){
+			deltaTime = Time.deltaTime;
+			int maxIndex = currDissolver.dissolveValue.Count-1;
+			
+			if(currDissolver.dissolveValue[maxIndex] >= 1f){
+				currDissolver.dissolve = false;
+				return;
+			} 
+
+			for(int i = 0;i<currDissolver.dissolveValue.Count;i++){
+				currDissolver.dissolveValue[i] += deltaTime * currDissolver.dissolveSpeed;
+			}
+			
+		}
+	}
+
+	void CheckUnDissolve()
+	{
+		if(currDissolver.unDissolve){
+			deltaTime = Time.deltaTime;
+			int maxIndex = currDissolver.dissolveValue.Count-1;
+			
+			if(currDissolver.dissolveValue[maxIndex] <= 0f){
+				currDissolver.unDissolve = false;
+				return;
+			} 
+
+			for(int i = 0;i<currDissolver.dissolveValue.Count;i++){
+				currDissolver.dissolveValue[i] -= deltaTime * currDissolver.dissolveSpeed;
+			}
+			
+		}
+	}
+}
