924a5630f401f18775a8f5458c531bbcacdc8937
DennisGL1 <dennis@gamelevelone.com>
2018-09-12 17:38:47 +0700

    - add bee asset - modified levels (adding more tree to fill the scene) - add tree system (randomize animation speed) - save scenes

---
+++ Assets/_MAIN/Scripts/Systems/Area/Environment/TreeSystem.cs
@@ -0,0 +1,37 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class TreeSystem : ComponentSystem {
+
+	public struct TreeComponent{
+		public readonly int Length;
+		public ComponentArray<Tree> tree;
+		public ComponentArray<Animator> treeAnim;
+	}
+
+	[InjectAttribute] public TreeComponent treeComponent;
+	public Tree currTree;
+	public Animator currTreeAnim;
+
+	protected override void OnUpdate()
+	{
+		for(int i = 0;i<treeComponent.Length;i++){
+			currTree = treeComponent.tree[i];
+			currTreeAnim = treeComponent.treeAnim[i];
+
+			InitTree();
+		}
+
+	}
+
+	void InitTree()
+	{
+		if(!currTree.initAnimSpeed){
+			currTree.initAnimSpeed = true;
+			float rnd = Random.Range(currTree.minAnimSpeed,currTree.maxAnimSpeed);
+			currTree.animSpeed = rnd;
+			currTreeAnim.speed = currTree.animSpeed;
+		}
+	}
+
+}
