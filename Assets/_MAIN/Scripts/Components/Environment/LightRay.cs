b1e27bd8e8eb3e163ca0297fc81b1647ee6c378f
DennisGL1 <dennis@gamelevelone.com>
2018-09-13 16:08:11 +0700

    - add shader (unlit with alpha) - add lightray system (lightray is hidden when nearby camera - modified scenes

---
+++ Assets/_MAIN/Scripts/Components/Environment/LightRay.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class LightRay : MonoBehaviour {
+	public Transform cameraTransform;
+	public List <MeshRenderer> lightRays;
+	public float distanceFactor = 3f;
+}
