7cdd9afa1577aef8f2ad74ba6956e832401e125c
DennisGL1 <dennis@gamelevelone.com>
2018-09-17 18:11:50 +0700

    - add bush entity and system - modified level 1-1 (more space) WIP

--- Assets/_MAIN/Scripts/Systems/Area/Environment/BushSystem.cs
+++ Assets/_MAIN/Scripts/Systems/Area/Environment/BushSystem.cs
@@ -1,16 +1,34 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using Unity.Entities;
 using UnityEngine;
 
-public class BushSystem : MonoBehaviour {
+public class BushSystem : ComponentSystem {
 
-	// Use this for initialization
-	void Start () {
-		
+	public struct BushComponent{
+		public readonly int Length;
+		public ComponentArray<Bush> bush;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	[InjectAttribute] BushComponent bushComponent;
+	Bush currBush;
+
+	protected override void OnUpdate()
+	{
+		for(int i = 0;i<bushComponent.Length;i++){
+			currBush = bushComponent.bush[i];
+
+			CheckBush();
+		}
+	}
+
+	void CheckBush()
+	{
+		if(currBush.destroy) Destroy();
+	}
+
+
+	void Destroy()
+	{
+		 GameObject.Destroy(currBush.gameObject);
+		UpdateInjectedComponentGroups();
 	}
 }
