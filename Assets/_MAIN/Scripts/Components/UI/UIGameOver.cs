6bfcf2de444934693c32b5864cacfbc87b3463b5
DennisGL1 <dennis@gamelevelone.com>
2018-09-24 06:48:06 +0700

    - add gameover entity and system - modified scene 3-1 (added stone obstacle to block portal to level 3-3) (add vine doors to block jatayu level)

---
+++ Assets/_MAIN/Scripts/Components/UI/UIGameOver.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UIGameOver : MonoBehaviour {
+	public CanvasGroup gameOverPanel;
+	public Animator gameOverAnim;
+	public Image buttonRestart;
+	public Image buttonReturnToTitle;
+	public GameObject black;
+
+	[HeaderAttribute("Current")]
+	public int selected = 0;
+	public bool call = false;
+	public bool endShow = false;
+	public bool endHide = false;
+	public bool doRestart = false;
+	public bool doReturnToTitle = false;
+
+	public void ButtonRestartOnClick()
+	{
+		doRestart = true;
+	}
+	public void ButtonReturnToTitleOnClick()
+	{
+		doReturnToTitle = true;
+	}
+
+	#region animation event
+	void EndShowing()
+	{
+		endShow = true;
+	}
+	void EndHiding()
+	{
+		endHide = true;
+	}
+	#endregion
+}
---
+++ Assets/_MAIN/Scripts/Systems/UI/UIGameOverSystem.cs
@@ -0,0 +1,101 @@
+﻿using Unity.Entities;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// to show UIGameOver: set uiGameOver.Call = true;
+/// </summary>
+public class UIGameOverSystem : ComponentSystem {
+
+	public struct UIGameOverComponent
+	{
+		public readonly int Length;
+		public ComponentArray<UIGameOver> uiGameOver;
+	}
+
+
+	[InjectAttribute]UIGameOverComponent uiGameOverComponent;
+	UIGameOver uiGameOver;
+
+	protected override void OnUpdate()
+	{
+		for(int i = 0;i<uiGameOverComponent.Length;i++){
+			uiGameOver = uiGameOverComponent.uiGameOver[i];
+
+			CheckCall();
+			CheckInput();
+			CheckSelected();
+			CheckButtonFunction();
+		}
+	}
+
+	void CheckCall()
+	{
+		if(uiGameOver.call){
+			uiGameOver.call = false;
+			uiGameOver.black.SetActive(true);
+			uiGameOver.gameOverAnim.SetBool("Show",true);
+		}
+	}
+
+	void CheckInput()
+	{
+		if(uiGameOver.endShow){
+			if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.DownArrow)){
+				if(uiGameOver.selected != 1){
+					uiGameOver.selected = 1;
+				}
+			}
+
+			if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+				if(uiGameOver.selected != 0){
+					uiGameOver.selected = 0;
+				}
+			}
+
+			if(Input.GetKeyDown(KeyCode.Return)){
+				if(uiGameOver.endShow){
+					uiGameOver.endShow = false;
+					uiGameOver.gameOverAnim.SetBool("Show",false);
+				}
+			}
+		}		
+	}
+
+	void CheckSelected()
+	{
+		if(uiGameOver.selected == 0){
+			uiGameOver.buttonRestart.color = Color.red;
+			uiGameOver.buttonReturnToTitle.color = Color.white;
+		}else if (uiGameOver.selected == 1){
+			uiGameOver.buttonRestart.color = Color.white;
+			uiGameOver.buttonReturnToTitle.color = Color.red;
+		}
+	}
+
+	void CheckButtonFunction()
+	{
+		if(uiGameOver.endHide){
+			uiGameOver.endHide = false;
+			if(uiGameOver.doRestart){
+				uiGameOver.doRestart = false;
+				Restart();
+			}
+
+			if(uiGameOver.doReturnToTitle){
+				uiGameOver.doReturnToTitle = false;
+				ReturnToTitle();
+			}
+		}
+	}
+
+	void Restart()
+	{
+		SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+
+	void ReturnToTitle()
+	{
+		SceneManager.LoadScene(Constants.SceneName.MAIN_MENU);
+	}
+}
