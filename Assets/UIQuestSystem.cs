7e20c5b14abc0f91be03fbb3c69ebc02976941e5
Bhakti <bhakti.gamelevelone@gmail.com>
2018-09-24 10:11:58 +0700

    WIP UIQuestSystem

---
+++ Assets/UIQuestSystem.cs
@@ -0,0 +1,34 @@
+﻿// using System.Collections;
+// using System.Collections.Generic;
+using UnityEngine;
+using Unity.Entities;
+
+public class UIQuestSystem : ComponentSystem {
+	public struct UIQuestData {
+		public readonly int Length;
+		public ComponentArray<UIQuest> UIQuest;
+	}
+	[InjectAttribute] UIQuestData uiQuestData;
+
+	UIQuest uiQuest;
+
+	protected override void OnUpdate () {
+		for (int i=0; i<uiQuestData.Length; i++) {
+			uiQuest = uiQuestData.UIQuest[i];
+
+			if (!uiQuest.isInitUIQuest) {
+				InitUIQuest();
+			}
+		}
+	}
+
+	void InitUIQuest () {
+		//
+
+		uiQuest.isInitUIQuest = true;
+	}
+
+	public void CheckIfAnyQuestIsDone () {
+		
+	}
+}
--- Assets/_MAIN/Scripts/Components/UI/UIQuest.cs
+++ Assets/_MAIN/Scripts/Components/UI/UIQuest.cs
@@ -2,8 +2,10 @@
 using UnityEngine.UI;
 
 public class UIQuest : MonoBehaviour {
-	public Text mainQuestText;
-	public Text[] sideQuestText;
+	// public Text mainQuestText;
+	public Text[] questTexts;
 
-	
+	[HeaderAttribute("Current")]
+	public bool isInitUIQuest;
+	public bool isModifyUI;
 }
