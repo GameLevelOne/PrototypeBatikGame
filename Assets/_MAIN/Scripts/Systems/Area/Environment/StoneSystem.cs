f435f80662d59d8dbfbcc483e10986cac8e698af
DennisGL1 <dennis@gamelevelone.com>
2018-09-27 16:22:38 +0700

    - modified stones prefab * destroyable with hammer * added particle and broken stone - turn off playerpref delete all

---
+++ Assets/_MAIN/Scripts/Systems/Area/Environment/StoneSystem.cs
@@ -0,0 +1,39 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class StoneSystem : ComponentSystem {
+	
+	public struct StoneComponent{
+		public readonly int Length;
+		public ComponentArray<Stone> stone;
+		public ComponentArray<Transform> stoneTransform;
+	}
+
+	[InjectAttribute] StoneComponent stoneComponent;
+	Stone stone;
+	Transform stoneTransform;
+
+	protected override void OnUpdate()
+	{
+		for(int i = 0;i<stoneComponent.Length;i++){
+			stone = stoneComponent.stone[i];
+			stoneTransform = stoneComponent.stoneTransform[i];
+
+			CheckStone();
+		}
+	}
+
+	void CheckStone()
+	{
+		if(stone.hit){
+			stone.hit = false;
+			if(stone.stoneGreyObj.activeSelf) stone.stoneGreyObj.SetActive(false);
+			stone.stoneColorObj.SetActive(false);
+			stone.stoneBrokenObj.SetActive(true);
+			stone.stoneCollider.enabled = false;
+			stone.stoneParticle.gameObject.SetActive(true);
+			stone.stoneParticle.Play();
+		}
+	}
+
+}
