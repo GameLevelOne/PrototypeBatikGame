ed7154f5f8a20dc7d32a3a862d19e945cd1df300
DennisGL1 <dennis@gamelevelone.com>
2018-09-28 15:38:01 +0700

    - added sound manager entity and system

---
+++ Assets/_MAIN/Scripts/Components/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+public enum BGM{
+
+}
+
+public enum SFX{
+
+}
+
+public class SoundManager : MonoBehaviour {
+
+#region SINGLETON
+	private static SoundManager instance = null;
+	public static SoundManager Instance {
+		get {
+			if (instance == null) {
+				instance = GameObject.FindObjectOfType<SoundManager>();
+			} 
+
+			return instance;
+		}
+	}
+
+	void Awake () {
+		if (instance != null && instance != this) {
+			GameObject.Destroy(gameObject);
+		}
+		DontDestroyOnLoad(gameObject);
+	}
+#endregion
+	public AudioSource bgmSource;
+	public AudioSource sfxSource;
+
+	public AudioClip[] BGMs;
+	public AudioClip[] SFXs;
+	[HeaderAttribute("Current")]
+	public bool init = false;
+}
