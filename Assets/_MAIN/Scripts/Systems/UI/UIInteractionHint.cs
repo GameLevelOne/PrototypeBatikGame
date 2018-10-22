2a7bd9989ee34a365caee6f00306ac163ed1defe
DennisGL1 <dennis@gamelevelone.com>
2018-10-22 16:35:41 +0700

    - add hint mechanic to player - apply prefab

---
+++ Assets/_MAIN/Scripts/Systems/UI/UIInteractionHint.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine.UI;
+using UnityEngine;
+
+public enum HintMessage{
+	OPEN,
+	LIFT
+}
+
+public class UIInteractionHint : MonoBehaviour {
+	public GameObject hintObj;
+	public Text textHint;
+	public string[] hintMessages;
+	
+	[HeaderAttribute("Current")]
+	public bool isActive = false;
+
+	public void ShowHint(HintMessage message)
+	{
+		if(!isActive){
+			textHint.text = hintMessages[(int)message];
+			hintObj.SetActive(true);
+			isActive = true;
+		}
+		if(isActive && message == HintMessage.OPEN){
+			textHint.text = hintMessages[(int)message];
+			hintObj.SetActive(true);
+		}
+		
+	}
+
+	public void HideHint()
+	{
+		isActive = false;
+		hintObj.SetActive(false);
+	}
+	
+}
