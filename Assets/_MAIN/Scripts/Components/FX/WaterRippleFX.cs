eba23cb4c4d69362bf38be0e38f7699696dffbd7
DennisGL1 <dennis@gamelevelone.com>
2018-10-23 12:52:10 +0700

    - added water ripple FX in scene jatayu - added water ripple when platform falls in timeline (scene jatayu)

---
+++ Assets/_MAIN/Scripts/Components/FX/WaterRippleFX.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class WaterRippleFX : MonoBehaviour {	
+	[HeaderAttribute("Current")]
+	public bool animDone = false;
+	public bool initDelay = false;
+	public float tDelay = 0f;
+	
+	void OnEndAnim()
+	{
+		animDone = true;
+	}
+}
---
+++ Assets/_MAIN/Scripts/Systems/FX/WaterRippleFXSystem.cs
@@ -0,0 +1,48 @@
+﻿using Unity.Entities;
+using UnityEngine;
+
+public class WaterRippleFXSystem : ComponentSystem {
+
+	public struct WaterRippleComponent{
+		public readonly int Length;
+		public ComponentArray<WaterRippleFX> waterRipple;
+		public ComponentArray<Animator> anim;
+	}
+
+	[InjectAttribute] WaterRippleComponent waterRippleComponent;
+	WaterRippleFX waterRipple;
+	Animator anim;
+
+	protected override void OnUpdate()
+	{
+		for(int i = 0;i<waterRippleComponent.Length;i++){
+			waterRipple = waterRippleComponent.waterRipple[i];
+			anim = waterRippleComponent.anim[i];
+
+			CheckAnim();
+		}
+	}
+
+	void CheckAnim()
+	{
+		if(waterRipple.animDone){
+			DoDelay();
+		}
+	}
+
+	void DoDelay()
+	{
+		if(!waterRipple.initDelay){
+			waterRipple.initDelay = true;
+			waterRipple.tDelay = Random.Range(3f,5f);
+		}else{
+			waterRipple.tDelay -= Time.deltaTime;
+			
+			if(waterRipple.tDelay <= 0f){
+				waterRipple.initDelay = false;
+				anim.Play("Animate",0,0f);
+				waterRipple.animDone = false;
+			}
+		}
+	}
+}
